ff91f11785c7104366d40ffa247314dc8836c676
Tiago-Salles <tiagosalles20@gmail.com>
2025-07-31 00:18:36 +0100

    ✨(dashboards) add Apache Superset
    - Added Apache Superset to the project using Docker.

--- my_financial/settings.py
+++ my_financial/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-&!41yrdpc#i3po@x5(xxml%+04@3u9b7k^8md_zf#0p6_fp3m0')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -95,6 +95,7 @@ DATABASES = {
     }
 }
 
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', None)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
